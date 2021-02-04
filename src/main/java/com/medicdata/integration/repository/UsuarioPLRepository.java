00bd95df39a417dc698eb8e3611701caceb7bdde
Alvaro Campos Vega <alvarocamposvega@gmail.com>
2021-02-04 13:47:05 +0100

    Creados Repositorios Capa Integracion

---
+++ src/main/java/com/medicdata/integration/repository/UsuarioPLRepository.java
@@ -0,0 +1,11 @@
+package com.medicdata.integration.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.medicdata.integration.model.UsuarioPL;
+
+@Repository
+public interface UsuarioPLRepository extends JpaRepository<UsuarioPL, Integer> {
+
+}
