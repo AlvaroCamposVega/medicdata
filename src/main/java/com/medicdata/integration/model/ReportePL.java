74ea1298e2a1b578b062b44256b73b8cc8ce6097
Alvaro Campos Vega <alvarocamposvega@gmail.com>
2021-02-04 14:11:10 +0100

    Anyadida Anotacion de Secuencia a la Id

--- src/main/java/com/medicdata/integration/model/ReportePL.java
+++ src/main/java/com/medicdata/integration/model/ReportePL.java
@@ -5,23 +5,35 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.TableGenerator;
 
 @Entity
 @Table(name="REPORTES")
 public class ReportePL {
 	
 	@Id
+	@TableGenerator(
+		name="REPORTES_SEQ",
+		table="REPORTES",
+		pkColumnName = "NOMBRE",
+		pkColumnValue="REPORTES_SEQ",
+		valueColumnName="GENERADOR",
+		allocationSize = 1
+	)
+	@GeneratedValue(generator="REPORTES_SEQ", strategy=GenerationType.TABLE)
 	private int id;
 	
 	@ManyToOne
 	@JoinColumn(name="ID_USUARIO")
 	private UsuarioPL usuario;
 	
-	@Column(name = "FECHA_REPORTE")
+	@Column(name="FECHA_REPORTE")
 	private Date fechaReporte;
 	
 	@Embedded
